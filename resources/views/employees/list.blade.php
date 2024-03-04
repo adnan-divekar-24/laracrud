b2dbdd21998f7da1a9c09f9e198622b960edc01d
adnandivekar <adnandivekar55@gmail.com>
2024-03-04 18:24:28 +0530

    changes in title

--- resources/views/employees/list.blade.php
+++ resources/views/employees/list.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SIMPLE LARAVEL 8 CRUD</title>
+    <title>SIMPLE LARAVEL 8 CRUD and this is title </title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
